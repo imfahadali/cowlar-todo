6877eee5e064163f5018c4c5e1a74f818115177e
Fahad Ali <fahad.ali@smodin.io>
2023-06-07 03:44:02 +0500

    Add Test Cases

--- client/src/pages/Todos.test.js
+++ client/src/pages/Todos.test.js
@@ -1,18 +1,16 @@
 import React from "react";
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  getByText,
-} from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 import Todos from "./Todos";
-import TodoItem from "../components/TodoItem";
 import useFetch from "../hooks/useFetch";
-import { postItem } from "../utils/api";
+import {
+  deleteItem,
+  postItem,
+  updateTodoCheck,
+  updateTodoName,
+} from "../services/api";
 
 let mockResponse = [
   { id: 1, name: "Todo 1", completed: false },
@@ -35,7 +33,7 @@ jest.mock("../hooks/useFetch", () => ({
   })),
 }));
 
-jest.mock("../utils/api", () => ({
+jest.mock("../services/api", () => ({
   __esModule: true,
   postItem: jest.fn(),
   deleteItem: jest.fn(),
@@ -59,6 +57,23 @@ describe("Todos", () => {
     expect(todo2).toHaveClass("line-through");
   });
 
+  test("delete todo", async () => {
+    deleteItem.mockResolvedValueOnce({ status: 200 });
+
+    fireEvent.click(screen.getByTestId("deletemenu-0"));
+    fireEvent.click(screen.getByTestId("deletebtn-0"));
+
+    await waitFor(() => {
+      expect(deleteItem).toHaveBeenCalledTimes(1);
+    });
+
+    // // Assert the updated component state
+    await waitFor(() => {
+      const deletedElement = screen.queryByText("Todo 1");
+      expect(deletedElement).not.toBeInTheDocument();
+    });
+  });
+
   test("add todo", async () => {
     useFetch.mockImplementation(() => ({
       response: [
@@ -70,18 +85,15 @@ describe("Todos", () => {
       loading: false,
       fetchData: mockFetchData,
     }));
-    postItem.mockResolvedValueOnce({
-      data: {
-        data: { _id: "3", name: "New Todo", completed: false },
-      },
-    });
+    postItem.mockResolvedValueOnce({ status: 200 });
 
     fireEvent.click(screen.getByTestId("addbtn"));
-    userEvent.type(screen.getByTestId("todo-field"), "New Todo");
+    const todoField = screen.getByTestId("todo-field");
+    userEvent.type(todoField, "New Todo");
     await waitFor(() => {
-      const inputField = screen.getByTestId("todo-field");
-      expect(inputField).toHaveValue("New Todo");
+      expect(todoField).toHaveValue("New Todo");
     });
+
     fireEvent.click(screen.getByTestId("postbtn"));
 
     // Expect the postItem function to be called
@@ -89,12 +101,40 @@ describe("Todos", () => {
       expect(postItem).toHaveBeenCalledTimes(1);
     });
 
-    // useFetch().fetchData();
-
-    // Expect the fetchData function to be called
     await waitFor(() => {
       expect(mockFetchData).toHaveBeenCalledTimes(1);
     });
-    // Expect the new todo to be in the document
+  });
+
+  test("edit todo", async () => {
+    updateTodoName.mockResolvedValueOnce({ status: 200 });
+
+    const todo2 = screen.getByText("Todo 2");
+    fireEvent.click(todo2);
+    const todo2InputField = screen.getByTestId("editfield-1");
+    userEvent.type(todo2InputField, "New Todo");
+    fireEvent.keyDown(todo2InputField, {
+      key: "Enter",
+      code: 13,
+      charCode: 13,
+    });
+
+    expect(updateTodoName).toHaveBeenCalledTimes(1);
+
+    // // // Assert the updated component state
+    await waitFor(() => {
+      const updatedTodo = screen.getByText("New Todo");
+      expect(updatedTodo).toBeInTheDocument();
+    });
+  });
+
+  test("check/uncheck todo", async () => {
+    updateTodoCheck.mockResolvedValueOnce({ status: 200 });
+
+    const checkbox = screen.getByTestId("checkbox-1");
+    expect(checkbox.checked).toEqual(true);
+    fireEvent.click(checkbox);
+
+    expect(updateTodoCheck).toHaveBeenCalledTimes(1);
   });
 });
